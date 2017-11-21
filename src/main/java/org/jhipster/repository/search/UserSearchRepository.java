8edc6a5653e660b1763b955cd5aaba2ea6f9ab47
naim mansor <naim_mansor95@yahoo.com>
2017-11-21 16:47:31 +0100

    Default

---
+++ src/main/java/org/jhipster/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package org.jhipster.repository.search;
+
+import org.jhipster.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
---
+++ src/main/java/org/jhipster/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package org.jhipster.repository.search;
